e55ffeff6dab3d42edadf23786a54ab36e113429
sepehr500 <sepehr411@gmail.com>
2022-03-12 22:58:44 -0500

    Add dotenv

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/sepehr500/dota-go
 go 1.17
 
 require github.com/google/go-querystring v1.1.0
+
+require github.com/joho/godotenv v1.4.0 // indirect
--- main.go
+++ main.go
@@ -2,17 +2,20 @@ package main
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/joho/godotenv"
 	dotago "github.com/sepehr500/dota-go/dota"
 )
-
-var key = ""
 
 func debugPrint(str interface{}) {
 	fmt.Printf("%+v\n", str)
 }
 
 func main() {
+	godotenv.Load()
+
+	var key = os.Getenv("DOTA_KEY")
 	client := dotago.New(key)
 	params := &dotago.MatchHistoryParams{AccountID: "41051979"}
 	result, _ := client.GetMatchHistory(params)
